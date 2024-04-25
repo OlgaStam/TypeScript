3ce4c2d9b3052c8b1ce0a755a43c2ddb508bbc8f
OlgaStam <newrealityforme@gmail.com>
2024-04-25 11:24:03 +0300

    basic

--- main.ts
+++ main.ts
@@ -1,1 +1,25 @@
-console.log("----Hello TypeScript")
+let strJS = 'any string'
+let strTS: string = 'any string'
+
+let numJS = 123
+let numTS: number = 123
+
+let boolJS = true
+let boolTS: boolean = true
+
+let anyType: any
+anyType = 1
+anyType = 'str'
+anyType = true
+anyType = []
+anyType = {}
+
+let strNumType: string | number
+strNumType = 'str'
+strNumType = 123
+
+let strNumTypeBool: string | number|boolean
+strNumTypeBool = 'str'
+strNumTypeBool = 123
+strNumTypeBool = true
+
