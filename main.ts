b3775bc4338dfc57c7fb384c1bc6f7fe50c72b88
OlgaStam <newrealityforme@gmail.com>
2024-04-25 13:29:07 +0300

    тип void

--- main.ts
+++ main.ts
@@ -22,29 +22,7 @@ log('Hi! ');
 //
 console.log('---------------');
 //
-// синтаксис ES5
-//
-// function sum(a: number, b: number): number {
-//   return a + b;
-// }
-// const  sum = function(a: number, b: number): number {
-//   return a + b;
-// }
-//
-// синтаксис ES6
-const sum1 = (a: number, b: number): number => a + b;
-console.log(sum1(1, 0));
-
-// будут проблемы с типизацией sum3
-// const sum2 = (a: string, b: number | string): number => +a + +b;
-// const sum3: (num1: number, num2: number) => number = sum2;
-
-// не будет проблем с типизацией sum3 и sum4
-const sum2 = (a: number | string, b: number | string): number => +a + +b;
-
-// тоже ошибка типизации
-// const sum: (a: number | string, b: number | string) => number = sum1;
-const sum3: (num1: string, num2: string) => number = sum2;
-const sum4: (a: number | string, b: number | string) => number = sum2;
-console.log(sum3('2', '0'));
-console.log(sum4('3', '0'));
+const con = (txt: string | number): void => console.log(txt);
+// для функции, которая ничего не возвращает, нет методов (после точки)
+con('str');
+con(123);
