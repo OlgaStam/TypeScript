e4d38c00d2cc20589867c84253f2fd8dc8677311
OlgaStam <newrealityforme@gmail.com>
2024-04-25 11:49:38 +0300

    методы по типам

--- main.js
+++ main.js
@@ -1,26 +1,11 @@
 "use strict";
-const numArrJS = [1, 2, 3];
-const numArrTS = [1, 2, 3];
-const numArrTS2 = [1, 2, 3];
-const strArrJS = ['1', '2', '3'];
-const strArrTS = ['1', '2', '3'];
-const strArrTS2 = ['1', '2', '3'];
-const boolArrJS = [true, false];
-const boolArrTS = [true, false];
-const boolArrTS2 = [true, false];
-const someTypesArrJS = [1, 'str', false];
-const someTypesArrTS = [
-    1,
-    'str',
-    false,
-    2,
-    false
-];
-const tupplesArrTS = [
-    1,
-    2,
-    3,
-    'str',
-    false
-];
-const anyArrTS = [{}, [], 3, 'str', false];
+const nameTS = 'Jhon';
+const ageTS = 25;
+function getNameTS() {
+    return nameTS;
+}
+function getAgeTS() {
+    return ageTS;
+}
+console.log('🚀 ~ getNameJS:', getNameTS().toUpperCase());
+console.log('🚀 ~ getAgeJS:', getAgeTS().toFixed(2));
--- main.ts
+++ main.ts
@@ -1,30 +1,11 @@
-const numArrJS = [1, 2, 3];
-const numArrTS: number[] = [1, 2, 3];
-const numArrTS2: Array<number> = [1, 2, 3];
+const nameTS: string = 'Jhon';
+const ageTS: number = 25;
 
-const strArrJS = ['1', '2', '3'];
-const strArrTS: string[] = ['1', '2', '3'];
-const strArrTS2: Array<string> = ['1', '2', '3'];
-
-const boolArrJS = [true, false];
-const boolArrTS: boolean[] = [true, false];
-const boolArrTS2: Array<boolean> = [true, false];
-
-const someTypesArrJS = [1, 'str', false];
-const someTypesArrTS: (number | string | boolean)[] = [
-  1,
-  'str',
-  false,
-  2,
-  false
-];
-
-const tupplesArrTS: [number, number, number, string, boolean] = [
-  1,
-  2,
-  3,
-  'str',
-  false
-];
-
-const anyArrTS: any[] = [{}, [], 3, 'str', false];
+function getNameTS(): string {
+  return nameTS;
+}
+function getAgeTS(): number {
+  return ageTS;
+}
+console.log('🚀 ~ getNameJS:', getNameTS().toUpperCase());
+console.log('🚀 ~ getAgeJS:', getAgeTS().toFixed(2));
