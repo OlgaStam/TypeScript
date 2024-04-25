62330479923b880961b7609e25997ac07cfef81b
OlgaStam <newrealityforme@gmail.com>
2024-04-25 15:51:18 +0300

    enam

--- main.js
+++ main.js
@@ -1,119 +1,25 @@
 "use strict";
-// объект на JS
-const personJS = {
-    name: 'Jhon',
-    age: 25,
-    gender: 'male',
-    skills: ['htm, css'],
-    print() {
-        console.log(this.name);
-    }
-};
-// и переиспользуем его
-const personTS = {
-    name: 'Jhon',
-    age: 25,
-    gender: 'male',
-    skills: ['html, css'],
-    getSkills() {
-        return this.skills;
-    }
-};
-const person2TS = {
-    name: 'Bob',
-    age: 31,
-    skills: ['htm, css, js, reackt'],
-    print() {
-        console.log(this.name);
-    }
-};
-// присвоим intersection type
-const dev1 = {
-    level: 'junior',
-    experiens: 5,
-    name: 'Bob',
-    age: 31,
-    skills: ['htm, css, js, reackt'],
-    print() {
-        console.log(this.name);
-    }
-};
-// вместо пересечения типов используем новый собранный тип
-const dev2 = {
-    level: 'junior',
-    experiens: 5,
-    name: 'Jhon',
-    age: 25,
-    skills: ['htm, css'],
-    getSkills() {
-        return this.skills;
-    }
-};
-// Partial<> - сопоставление
-// пометить все поля необязательными
-const dev3 = {
-    level: 'junior',
-    name: 'Jhon'
-};
-// Вот некоторые примеры расширенных типов в TypeScript:
-// Union Types (Объединенные типы): Позволяют определить переменную или параметр функции, который может принимать значения из нескольких типов. Например: number | string.
-// Intersection Types (Пересекающиеся типы): Позволяют комбинировать несколько типов таким образом, что результат будет содержать все члены из каждого типа. Например: TypeA & TypeB.
-// Типы-псевдонимы (Type Aliases): Позволяют создавать пользовательские именованные типы для повторного использования. Например: type MyType = { prop: string }.
-// Типы перечисления (Enum Types): Позволяют создавать набор именованных константных значений. Например: enum Color { Red, Green, Blue }.
-// Типы сопоставления (Mapped Types): Позволяют создавать новые типы на основе существующих типов с помощью оператора keyof. Например: type Optional<T> = { [K in keyof T]?: T[K] }.
-// Типы совпадения шаблона (Template Literal Types): Позволяют создавать новые типы, объединяя строки с помощью шаблонных литералов. Например: type Greeting = Hello, ${string};.
-// Типы перегрузки (Function Overload Types): Позволяют определять несколько вариантов сигнатур функции для различных комбинаций типов параметров и возвращаемого значения.Например:
-// typescript
-// Copy code
-// function process(value: string): number;
-// function process(value: number): string;
-// function process(value: string | number): string | number {
-// Реализация функции
-// }
-// Abstract Classes (Абстрактные классы): Позволяют определять общую структуру для классов, но не могут быть непосредственно инстанциированы. Например:
-// typescript
-// Copy code
-// abstract class Animal {
-//     abstract makeSound(): void;
-// }
-// class Dog extends Animal {
-//     makeSound() {
-//         console.log('Woof');
-//     }
-// }
-// const dog = new Dog();
-// dog.makeSound(); // Output: Woof
-// Типы невозможности (Never Type): Представляет тип значения, которое никогда не должно произойти. Этот тип обычно используется для обработки ошибок или ситуаций, которые не должны возникать в нормальном выполнении программы. Например:
-// typescript
-// Copy code
-// function throwError(message: string): never {
-//     throw new Error(message);
-// }
-// Собственные индексы (Index Signatures): Позволяют определять типы для динамических свойств объектов. Например:
-// typescript
-// Copy code
-// interface MyDictionary {
-//     [key: string]: number;
-// }
-// const dict: MyDictionary = {
-//     one: 1,
-//     two: 2,
-//     three: 3,
-// };
-// Readonly Properties (Свойства только для чтения): Позволяют указать, что свойство объекта доступно только для чтения после его инициализации. Например:
-// typescript
-// Copy code
-// interface Point {
-//     readonly x: number;
-//     readonly y: number;
-// }
-// const p: Point = { x: 10, y: 20 };
-// p.x = 5; // Ошибка: Невозможно присвоить новое значение свойству "x"
-// Index Types (Индексные типы): Позволяют определить тип значений, которые могут быть доступны через индексацию объекта. Например:
-// typescript
-// Copy code
-// interface StringArray {
-//     [index: number]: string;
-// }
-// const myArray: StringArray = ['a', 'b', 'c'];
-// const firstElement: string = myArray[0]; // 'a'
+const arr = ['html', 'css', 'js', 'ts', 'git', 'github', 'vscode'];
+console.log('arr:', arr);
+console.log('arr[0]:', arr[0]);
+console.log('arr[2]:', arr[2]);
+//  Перечисления - позволяет нам определять набор именованных числовых констант и определяется при помощи ключевого слова Enum. Являются подтипами примитивного типа Num
+var Skills;
+(function (Skills) {
+    Skills[Skills["HTML"] = 0] = "HTML";
+    Skills[Skills["CSS"] = 1] = "CSS";
+    Skills[Skills["JS"] = 6] = "JS"; //демонтсрация ниже ,
+    Skills[Skills["TS"] = 7] = "TS";
+    Skills[Skills["Git"] = 8] = "Git";
+    Skills[Skills["Github"] = 9] = "Github";
+    Skills[Skills["VSCode"] = 10] = "VSCode";
+})(Skills || (Skills = {}));
+//  в JS мы получим объект, который указывает сам на себя
+console.log('Skills:', Skills);
+console.log('Skills[0]: ', Skills[0]);
+console.log('Skills.HTML:', Skills.HTML);
+console.log('Skills[2]:', Skills[2]); //Skills[2]: undefined* потому что JS = 6, а не 2
+console.log('Skills.JS:', Skills.JS); //Skills.JS: 6 
+console.log('arr[Skills.HTML]:', arr[Skills.HTML]);
+console.log('arr[Skills.JS]:', arr[Skills.JS] //arr[Skills.JS]: vscode, потому что в массиве "arr" в позиции "6" находится "vscode" 
+);
--- main.ts
+++ main.ts
@@ -1,174 +1,32 @@
-// объект на JS
-const personJS = {
-  name: 'Jhon',
-  age: 25,
-  gender: 'male',
-  skills: ['htm, css'],
-  print() {
-    console.log(this.name);
-  }
-};
-// объект на TS с типизацией
-// const personTS: {
-//   name: string;
-//   age: number;
-//   gender: string;
-//   skills: string[];
-//   print: () => void;
-// } = {
-//   name: 'Jhon',
-//   age: 25,
-//   gender: 'male',
-//   skills: ['htm, css'],
-//   print() {
-//     console.log(this.name);
-//   }
-// };
-// const personTS2: {
-//   name: string;
-//   age: number;
-//   gender: string;
-//   skills: string[];
-//   print: () => void;
-// } = {
-//   name: 'Jhon',
-//   age: 25,
-//   gender: 'male',
-//   skills: ['htm, css'],
-//   print() {
-//     console.log(this.name);
-//   }
-// };
-//
-// создадим собственный универсальный тип с необязательными полями
-type Person = {
-  name: string;
-  age: number;
-  gender?: string;
-  skills: string[];
-  print?: () => void;
-  getSkills?: () => string[];
-};
-// и переиспользуем его
-const personTS: Person = {
-  name: 'Jhon',
-  age: 25,
-  gender: 'male',
-  skills: ['html, css'],
-  getSkills(): string[] {
-    return this.skills;
-  }
-};
-const person2TS: Person = {
-  name: 'Bob',
-  age: 31,
-  skills: ['htm, css, js, reackt'],
-  print() {
-    console.log(this.name);
-  }
-};
-type Developer = {
-  level: string;
-  experiens: number;
-};
-// присвоим intersection type
-const dev1: Person & Developer = {
-  level: 'junior',
-  experiens: 5,
-  name: 'Bob',
-  age: 31,
-  skills: ['htm, css, js, reackt'],
-  print() {
-    console.log(this.name);
-  }
-};
+const arr = ['html', 'css', 'js', 'ts', 'git', 'github', 'vscode'];
+console.log('arr:', arr);
+console.log('arr[0]:', arr[0]);
+console.log('arr[2]:', arr[2]);
 
-type Advanced = Person & Developer;
-// вместо пересечения типов используем новый собранный тип
+//  Перечисления - позволяет нам определять набор именованных числовых констант и определяется при помощи ключевого слова Enum. Являются подтипами примитивного типа Num
 
-const dev2: Advanced = {
-  level: 'junior',
-  experiens: 5,
-  name: 'Jhon',
-  age: 25,
-  skills: ['htm, css'],
-  getSkills(): string[] {
-    return this.skills;
-  }
-};
+enum Skills {
+  HTML,
+  CSS,
+  JS = 6 //демонтсрация ниже ,
+  TS,
+  Git,
+  Github,
+  VSCode
+}
 
-// Partial<> - сопоставление
-// пометить все поля необязательными
-const dev3: Partial<Advanced> = {
-  level: 'junior',
-  name: 'Jhon'
-};
+//  в JS мы получим объект, который указывает сам на себя
+console.log('Skills:', Skills);
+console.log('Skills[0]: ', Skills[0]);
+console.log('Skills.HTML:', Skills.HTML);
+console.log(
+  'Skills[2]:',
+  Skills[2]
+); //Skills[2]: undefined* потому что JS = 6, а не 2
+console.log('Skills.JS:', Skills.JS); //Skills.JS: 6 
 
-// Вот некоторые примеры расширенных типов в TypeScript:
-
-// Union Types (Объединенные типы): Позволяют определить переменную или параметр функции, который может принимать значения из нескольких типов. Например: number | string.
-// Intersection Types (Пересекающиеся типы): Позволяют комбинировать несколько типов таким образом, что результат будет содержать все члены из каждого типа. Например: TypeA & TypeB.
-// Типы-псевдонимы (Type Aliases): Позволяют создавать пользовательские именованные типы для повторного использования. Например: type MyType = { prop: string }.
-// Типы перечисления (Enum Types): Позволяют создавать набор именованных константных значений. Например: enum Color { Red, Green, Blue }.
-// Типы сопоставления (Mapped Types): Позволяют создавать новые типы на основе существующих типов с помощью оператора keyof. Например: type Optional<T> = { [K in keyof T]?: T[K] }.
-// Типы совпадения шаблона (Template Literal Types): Позволяют создавать новые типы, объединяя строки с помощью шаблонных литералов. Например: type Greeting = Hello, ${string};.
-// Типы перегрузки (Function Overload Types): Позволяют определять несколько вариантов сигнатур функции для различных комбинаций типов параметров и возвращаемого значения.Например:
-// typescript
-// Copy code
-// function process(value: string): number;
-// function process(value: number): string;
-// function process(value: string | number): string | number {
-// Реализация функции
-// }
-// Abstract Classes (Абстрактные классы): Позволяют определять общую структуру для классов, но не могут быть непосредственно инстанциированы. Например:
-// typescript
-// Copy code
-// abstract class Animal {
-//     abstract makeSound(): void;
-// }
-
-// class Dog extends Animal {
-//     makeSound() {
-//         console.log('Woof');
-//     }
-// }
-
-// const dog = new Dog();
-// dog.makeSound(); // Output: Woof
-// Типы невозможности (Never Type): Представляет тип значения, которое никогда не должно произойти. Этот тип обычно используется для обработки ошибок или ситуаций, которые не должны возникать в нормальном выполнении программы. Например:
-// typescript
-// Copy code
-// function throwError(message: string): never {
-//     throw new Error(message);
-// }
-// Собственные индексы (Index Signatures): Позволяют определять типы для динамических свойств объектов. Например:
-// typescript
-// Copy code
-// interface MyDictionary {
-//     [key: string]: number;
-// }
-
-// const dict: MyDictionary = {
-//     one: 1,
-//     two: 2,
-//     three: 3,
-// };
-// Readonly Properties (Свойства только для чтения): Позволяют указать, что свойство объекта доступно только для чтения после его инициализации. Например:
-// typescript
-// Copy code
-// interface Point {
-//     readonly x: number;
-//     readonly y: number;
-// }
-
-// const p: Point = { x: 10, y: 20 };
-// p.x = 5; // Ошибка: Невозможно присвоить новое значение свойству "x"
-// Index Types (Индексные типы): Позволяют определить тип значений, которые могут быть доступны через индексацию объекта. Например:
-// typescript
-// Copy code
-// interface StringArray {
-//     [index: number]: string;
-// }
-
-// const myArray: StringArray = ['a', 'b', 'c'];
-// const firstElement: string = myArray[0]; // 'a'
+console.log('arr[Skills.HTML]:', arr[Skills.HTML]);
+console.log(
+  'arr[Skills.JS]:',
+  arr[Skills.JS]//arr[Skills.JS]: vscode, потому что в массиве "arr" в позиции "6" находится "vscode" 
+); 
