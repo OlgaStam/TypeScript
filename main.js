df97748c479a00b7b3044e86673a1277adf79d34
OlgaStam <newrealityforme@gmail.com>
2024-04-26 09:18:43 +0300

    interface implements

--- main.js
+++ main.js
@@ -9,13 +9,55 @@ const obj = { name: 'Object name', length: 20 };
 getLength('any string'); //10
 getLength([1, 2, 3]); //3
 getLength(obj); //20
-const person = {
-    name: 'Jhon',
-    age: 25,
-    gender: 'male',
-    greet(msg) {
-        console.log(`${msg}, ${this.name}!`);
+// в этом коде ошибка - нет описанного св-ва
+// const people = { name: 'Jhon', age: 20 };
+// getLength(people);
+console.log('------------------');
+// чтобы ОБЯЗАТЬ класс реализовать определенные свойства-методы ИМПЛЕМЕНТИРУЮТ соответсвтующие интерфейсы
+class Person {
+    constructor() {
+        this.name = 'Jhon';
+        this.age = 25;
+        this.gender = 'male';
+        // можем реализовать свойства, не указанные в интерфейсе
+        this.city = 'Kharkiv';
+        this.skills = ['html', 'css'];
     }
-};
-console.log('person:', person);
-person.greet('Hi');
+    getSkills() {
+        return this.skills;
+    }
+    greet(msg) {
+        console.log('Hi!');
+    }
+}
+const person = new Person();
+class Rectangle {
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+        this.name = 'Rectangle';
+    }
+    printArea() {
+        const area = this.width * this.height;
+        return `Area of a ${this.name} is ${area}`;
+    }
+}
+class Circle {
+    constructor(radius) {
+        this.radius = radius;
+        this.name = 'Circle';
+    }
+    printArea() {
+        const area = +(Math.PI * this.radius ** 2).toFixed(2);
+        return `Area of a ${this.name} is ${area}`;
+    }
+}
+const rectangle = new Rectangle(100, 200);
+const circle = new Circle(30);
+console.log(rectangle.printArea());
+console.log(circle.printArea());
+console.log('------------------');
+const shapes = [];
+shapes[0] = rectangle;
+shapes[1] = circle;
+shapes.forEach((shape) => console.log(shape.printArea()));
--- main.ts
+++ main.ts
@@ -17,28 +17,86 @@ getLength(obj); //20
 // const people = { name: 'Jhon', age: 20 };
 // getLength(people);
 
+console.log('------------------');
 // -----------------
 
-// interface PersonInterface {
+// type PersonType = {
 //   name: string;
 //   age: number;
 //   gender: string;
 //   greet(msg: string): void;
-// }
-type PersonType = {
+// };
+// const person: PersonType = {
+//   name: 'Jhon',
+//   age: 25,
+//   gender: 'male',
+//   greet(msg: string): void {
+//     console.log(`${msg}, ${this.name}!`);
+//   }
+// };
+
+interface PersonInterface {
   name: string;
   age: number;
   gender: string;
   greet(msg: string): void;
-};
+}
+interface GetSkillsInterface {
+  skills: string[];
+  getSkills(): string[];
+}
+// чтобы ОБЯЗАТЬ класс реализовать определенные свойства-методы ИМПЛЕМЕНТИРУЮТ соответсвтующие интерфейсы
+class Person implements PersonInterface, GetSkillsInterface {
+  name: string = 'Jhon';
+  age: number = 25;
+  gender: string = 'male';
 
-const person: PersonType = {
-  name: 'Jhon',
-  age: 25,
-  gender: 'male',
-  greet(msg: string): void {
-    console.log(`${msg}, ${this.name}!`);
+  // можем реализовать свойства, не указанные в интерфейсе
+  city: string = 'Kharkiv';
+
+  skills: string[] = ['html', 'css'];
+  getSkills(): string[] {
+    return this.skills;
   }
-};
-console.log('person:', person);
-person.greet('Hi');
+  greet(msg: string): void {
+    console.log('Hi!');
+  }
+}
+const person = new Person();
+// console.log('person:', person);
+// console.log('person.getSkills():', person.getSkills());
+// ------------------
+interface ShapeInterface {
+  name: string;
+  printArea: () => string;
+}
+class Rectangle implements ShapeInterface {
+  name: string = 'Rectangle';
+  constructor(private width: number, private height: number) {}
+
+  printArea(): string {
+    const area: number = this.width * this.height;
+    return `Area of a ${this.name} is ${area}`;
+  }
+}
+
+class Circle implements ShapeInterface {
+  name: string = 'Circle';
+  constructor(private radius: number) {}
+
+  printArea(): string {
+    const area: number = +(Math.PI * this.radius ** 2).toFixed(2);
+    return `Area of a ${this.name} is ${area}`;
+  }
+}
+
+const rectangle = new Rectangle(100, 200);
+const circle = new Circle(30);
+
+console.log(rectangle.printArea());
+console.log(circle.printArea());
+console.log('------------------');
+const shapes: ShapeInterface[] = [];
+shapes[0] = rectangle;
+shapes[1] = circle;
+shapes.forEach((shape: ShapeInterface) => console.log(shape.printArea()));
