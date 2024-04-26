1fae843210f85603f447dbb7344d04d79281cf41
OlgaStam <newrealityforme@gmail.com>
2024-04-26 11:19:39 +0300

    декораторы - обертки

--- main.js
+++ main.js
@@ -5,18 +5,16 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-function consoleLog(param) {
-    console.log(param);
+function consoleLog(constrClass) {
+    console.log(constrClass);
 }
-consoleLog(123);
-// в консоли выводится
-// 123
-console.log('consoleLog:', consoleLog);
-// в консоли выводится
-// consoleLog: ƒ consoleLog(param) {
-//   console.log(param);
-// }
-// добавили @ - функция стала ДЕКОРАТОРОМ и получила значение функции, к которой она прикреплена (т.е. сразу на следующей строке после декоратора идет его класс)
+function conditionLog(flag = false) {
+    console.log(flag);
+    // замыкание получает внутрь себя декоратор при условии флаг=истина
+    return flag ? consoleLog : null;
+}
+// conditionLog(true); // со скобками обычная функция
+// @conditionLog // декоратор - без скобок и сразу перед классом
 let Person = class Person {
     constructor(name, age) {
         this.name = name;
@@ -25,16 +23,6 @@ let Person = class Person {
     }
 };
 Person = __decorate([
-    consoleLog
+    conditionLog(true) // со скобками все еще функция, но уже является оберткой для настоящего декоратора
 ], Person);
-// в консоли выводится
-// class Person {
-//   constructor(name, age) {
-//       this.name = name;
-//       this.age = age;
-//       console.log('Hello from Person Constructor');
-//   }
-// }
 const person = new Person('Jhon', 25);
-// в консоли выводится
-// Hello from Person Constructor
--- main.ts
+++ main.ts
@@ -1,32 +1,20 @@
-function consoleLog(param: any) {
-  console.log(param);
+function consoleLog(constrClass: Function) {
+  console.log(constrClass);
 }
 
-consoleLog(123);
-// в консоли появилось
-// 123
-console.log('consoleLog:', consoleLog);
-// в консоли появилось
-// consoleLog: ƒ consoleLog(param) {
-//   console.log(param);
-// }
+function conditionLog(flag: boolean = false): any {
+  console.log(flag);
+  // замыкание получает внутрь себя декоратор при условии флаг=истина
+  return flag ? consoleLog : null;
+}
 
-// добавили @ - функция стала ДЕКОРАТОРОМ и получила значение функции, к которой она прикреплена (т.е. сразу на следующей строке после декоратора идет его класс)
-@consoleLog
+// conditionLog(true); // со скобками обычная функция
+// @conditionLog // декоратор - без скобок и сразу перед классом
+@conditionLog(true) // со скобками все еще функция, но уже является оберткой для настоящего декоратора
 class Person {
   constructor(public name: string, public age: number) {
     console.log('Hello from Person Constructor');
   }
 }
-// в консоли появилось
-// class Person {
-//   constructor(name, age) {
-//       this.name = name;
-//       this.age = age;
-//       console.log('Hello from Person Constructor');
-//   }
-// }
 
 const person = new Person('Jhon', 25);
-// в консоли появилось
-// Hello from Person Constructor
