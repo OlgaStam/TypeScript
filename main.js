dc1ad6c38035bf1cb95865d0573cfd9faa064d13
OlgaStam <newrealityforme@gmail.com>
2024-04-26 12:20:17 +0300

    namespace

--- main.js
+++ main.js
@@ -1,74 +1,29 @@
 "use strict";
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-function AddPrinter(constrClass) {
-    console.dir(constrClass);
-    constrClass.prototype.printInfo = function () {
-        const p = document.createElement('p');
-        p.innerHTML = `${JSON.stringify(this)}<br> 
-    Имя: ${this.name}<br>
-    Возраст: ${this.age}<br>
-    Пол: ${this.gender}<br>
-    Город: ${this.location}<br>
-    `;
-        document.body.append(p);
-    };
-}
-let Developer = class Developer {
-    constructor(name, age, gender, location) {
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
-        this.location = location;
+let PI = 'pi';
+let isEmpty = '';
+// code
+// code
+// code
+// code
+// code
+// code
+// code
+// code
+// code
+// code
+// code
+// code
+// code
+// code
+var Util;
+(function (Util) {
+    Util.PI = 3.14;
+    function isEmpty(d) {
+        return !d;
     }
-};
-Developer = __decorate([
-    AddPrinter
-], Developer);
-const developer = new Developer('John', 25, 'male', 'New York');
-developer.printInfo();
-// -----------------
-function Override(label) {
-    return function (target, key) {
-        // console.log('🚀 ~ key:', key);
-        // console.log('🚀 ~ target:', target);
-        Object.defineProperty(target, key, {
-            configurable: false,
-            get: () => label,
-            set: (v) => { }
-        });
-    };
-}
-function ReadOnly(target, key) {
-    // console.log('🚀 ~ key:', key);
-    // console.log('🚀 ~ target:', target);
-    Object.defineProperty(target, key, {
-        // value: 'Default value',
-        get: () => 'Default value',
-        // writable: false
-        set: (v) => { }
-    });
-}
-class Test {
-    constructor() {
-        // @Override('Hello world')
-        this.castomKey = 'any string';
-    }
-}
-__decorate([
-    ReadOnly //без скобок - таргет и ключ получаем напрямую, минуя функцию - обертку
-], Test.prototype, "castomKey", void 0);
-const t = new Test();
-// если декоратор выключить   // @Override('Hello world')
-// console.log(t.castomKey); //any string
-// t.castomKey = 'new string name';
-// console.log(t.castomKey); //new string name
-// если декоратор включить   @Override('Hello world') - свойство закрыто от записи, получило некое значение
-// по сути класс запечатан с помощью замены базового значения
-console.log(t.castomKey); //Hello world
-t.castomKey = 'new string name';
-console.log(t.castomKey); //Hello world
+    Util.isEmpty = isEmpty;
+})(Util || (Util = {}));
+console.log('🚀 ~ PI:', PI);
+console.log('🚀 ~ isEmpty:', isEmpty);
+// namespaсе - позволяет сгруппировать в определенную область имен нужные константы, функции и пользоваться ими
+// т.е. возможность создать именованую область и потом с ней работать
--- main.ts
+++ main.ts
@@ -1,70 +1,28 @@
-function AddPrinter(constrClass: Function) {
-  console.dir(constrClass);
-  constrClass.prototype.printInfo = function () {
-    const p = document.createElement('p');
+let PI = 'pi';
+let isEmpty = '';
 
-    p.innerHTML = `${JSON.stringify(this)}<br> 
-    Имя: ${this.name}<br>
-    Возраст: ${this.age}<br>
-    Пол: ${this.gender}<br>
-    Город: ${this.location}<br>
-    `;
+// code
+// code
+// code
+// code
+// code
+// code
+// code
+// code
+// code
+// code
+// code
+// code
+// code
+// code
 
-    document.body.append(p);
-  };
+namespace Util {
+  export const PI = 3.14;
+  export function isEmpty(d: any) {
+    return !d;
+  }
 }
-
-@AddPrinter
-class Developer {
-  constructor(
-    public name: string,
-    public age: number,
-    public gender: string,
-    public location: string
-  ) {}
-}
-const developer: any = new Developer('John', 25, 'male', 'New York');
-developer.printInfo();
-
-// -----------------
-
-function Override(label: string) {
-  return function (target: any, key: string) {
-    // console.log('🚀 ~ key:', key);
-    // console.log('🚀 ~ target:', target);
-    Object.defineProperty(target, key, {
-      configurable: false,
-      get: () => label,
-      set: (v) => {}
-    });
-  };
-}
-
-function ReadOnly(target: any, key: string) {
-  // console.log('🚀 ~ key:', key);
-  // console.log('🚀 ~ target:', target);
-  Object.defineProperty(target, key, {
-    // value: 'Default value',
-    get: () => 'Default value',
-    // writable: false
-    set: (v) => {}
-  });
-}
-
-class Test {
-  // @Override('Hello world')
-  @ReadOnly //без скобок - таргет и ключ получаем напрямую, минуя функцию - обертку
-  castomKey: string = 'any string';
-}
-
-const t = new Test();
-// если декоратор выключить   // @Override('Hello world')
-// console.log(t.castomKey); //any string
-// t.castomKey = 'new string name';
-// console.log(t.castomKey); //new string name
-
-// если декоратор включить   @Override('Hello world') - свойство закрыто от записи, получило некое значение
-// по сути класс запечатан с помощью замены базового значения
-console.log(t.castomKey); //Hello world
-t.castomKey = 'new string name';
-console.log(t.castomKey); //Hello world
+console.log('🚀 ~ PI:', PI);
+console.log('🚀 ~ isEmpty:', isEmpty);
+// namespaсе - позволяет сгруппировать в определенную область имен нужные константы, функции и пользоваться ими
+// т.е. возможность создать именованую область и потом с ней работать
